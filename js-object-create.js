3d5779aa11d2be4de9f1ac2d7ed6fecd74766b60
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-02-03 16:02:29 +0530

    upload

---
+++ js-object-create.js
@@ -0,0 +1,296 @@
+let mybike={
+	model: 2002,
+	number: 1234,
+	company: "rk",
+	color: "orange",
+	speed: "500km/hr",
+	value: "50years",
+}
+document.write(JSON.stringify(mybike));
+console.log(mybike);
+console.log(mybike.number);
+console.log(mybike["model"]);
+document.write("<br>");
+document.write("<br>");
+
+let appa={
+	appa: "selavamani",
+	age: 50,
+	gender: "male",
+	work: "driver",
+	salary: 30000,
+	place: "TVNR",
+}
+document.write(JSON.stringify(appa));
+console.log(appa);
+console.log(appa.appa);
+console.log(appa["age"]);
+document.write("<br>");
+document.write("<br>");
+
+let student={
+	name:"rajesh",
+	age: 21,
+	gender:"male",
+	class:12,
+	number:123,
+	velage: "TVNR",
+}
+document.write(JSON.stringify(student));
+console.log(student);
+console.log(student.age);
+console.log(student["name"]);
+document.write("<br>");
+document.write("<br>");
+let class1={
+	student: 30,
+	female: 4,
+	male: 26,
+	study: "javascript",
+	work: "best company",
+	salary: 15000,
+}
+document.write(JSON.stringify(class1));
+console.log(class1);
+console.log(class1.study);
+console.log(class1["work"]);
+document.write("<br>");
+document.write("<br>");
+let room={
+	html: "completed",
+	css: "completed",
+	bootsrap: "completed",
+	month: 4,
+	next: "job",
+	javascript: 25,
+	
+}
+
+document.write(JSON.stringify(room));
+console.log(room);
+console.log(room.css);
+console.log(room["month"]);
+document.write("<br>");
+document.write("<br>");
+
+let dog={
+	dog: "animal",
+	age: 3,
+	color: "black",
+	rs: 1500,
+	name: "kutty",
+	skill: "speed run",
+	
+}
+document.write(JSON.stringify(dog));
+console.log(dog);
+console.log(dog.name);
+console.log(dog["dog"]);
+document.write("<br>");
+document.write("<br>");
+let byCicle={
+	color: "black",
+	model: 123,
+	speed: "40km/hr",
+	look: "super",
+	break: "super",
+	number: 4783,
+	
+}
+
+document.write(JSON.stringify(byCicle));
+console.log(byCicle);
+console.log(byCicle.speed);
+console.log(byCicle["color"]);
+document.write("<br>");
+document.write("<br>");
+let bag={
+	color:"red",
+	type:"collage",
+	zip: 5,
+	weight: 500,
+	length: 700,
+	strong:"1molebower", 
+}
+document.write(JSON.stringify(bag));
+console.log(bag);
+console.log(bag.zip);
+console.log(bag["type"]);
+document.write("<br>");
+document.write("<br>");
+
+let ring={
+	model: "round",
+	ring: "gold",
+	inch: 2.7,
+	color: "red",
+	company: 916,
+	stop: "tenkasi gomathi",
+}
+document.write(JSON.stringify(ring));
+console.log(ring);
+console.log(ring.ring);
+console.log(ring["stop"]);
+document.write("<br>");
+document.write("<br>");
+let world={
+	world:"night mode",
+	color: "multi",
+	size: "round",
+	people: 12390599-46,
+	man: 3458067,
+	woman: 750286,
+}
+document.write(JSON.stringify(world));
+console.log(world);
+console.log(world.people);
+console.log(world["man"]);
+document.write("<br>");
+document.write("<br>");
+
+let car={
+	model:"car",
+	number: 4567,
+	color:"black",
+	company: "rolls rayos",
+	year: "100%",
+	speed: "2000km/hr",
+}
+document.write(JSON.stringify(car));
+console.log(car);
+console.log(car.company);
+console.log(car["speed"]);
+document.write("<br>");
+document.write("<br>");
+let mobile={
+	model: "z1 bro",
+	color: "blue",
+	phonenum:6379193920,
+	company:"vivo",
+	lange:1019,
+	yearold: 4,
+}
+document. write(JSON.stringify(mobile));
+console.log(mobile);
+console.log(mobile.phonenum);
+console.log(mobile["yearold"]);
+document.write("<br>");
+document.write("<br>");
+
+let book={
+	model: "squre",
+	pappercolor: "white",
+	bookcolor:"orange",
+	height: "30cm",
+	page: 124,
+	weight: "10cm",
+	
+}
+document.write(JSON.stringify(book));
+console.log(book);
+console.log(book.page);
+console.log(book["bookcolor"]);
+document.write("<br>");
+document.write("<br>");
+let company={
+	name: "rk",
+	sale: "sprolina",
+	kg1rs:800,
+	make: "own",
+	location: "TVNR",
+	add:"no",
+}
+document.write(JSON.stringify(company));
+console.log(company);
+console.log(company.location);
+console.log(company["sale"]);
+document.write("<br>");
+document.write("<br>");
+
+let stop={
+	ideam: "all",
+	stopname:" rk-stop",
+	code:"45",
+	location:"world",
+	owerpnumber:776655443322,
+	id:1234,
+}
+
+document.write(JSON.stringify(stop));
+console.log(stop);
+console.log(stop.code);
+console.log(stop["owerpnumber"]);
+document.write("<br>");
+document.write("<br>");
+let shoes={
+	prand:"aadi",
+	size:8,
+	model:"nife",
+	color:"black",
+	weight:500,
+	totalkm: "400km"
+}
+
+document.write(JSON.stringify(shoes));
+console.log(shoes);
+console.log(shoes.totalkm);
+console.log(shoes["model"]);
+document.write("<br>");
+document.write("<br>");
+
+let TV={
+	size:32,
+	model:"LED",
+	color: "black",
+	prand: "sony",
+	position: "top",
+	network: "yes",
+}
+document.write(JSON.stringify(TV));
+console.log(TV);
+console.log(TV.prand);
+console.log(TV["size"]);
+document.write("<br>");
+document.write("<br>");
+let address={
+	road:"main",
+	location:"tamilnadu",
+	area:"tenkasi",
+	cite:"TVNR",
+	dnumber:"4/152-1",
+	mumber: "rk",
+}
+document.write(JSON.stringify(address));
+console.log(address);
+console.log(address.area);
+console.log(address["mumber"]);
+document.write("<br>");
+document.write("<br>");
+let life={
+	model: "appa amma",
+	work: "any",
+	place: "any",
+	salary: 15000,
+	dream:"get car",
+	gender:"male",
+}
+document.write(JSON.stringify(life));
+console.log(life);
+console.log(life.salary);
+console.log(life["gender"]);
+document.write("<br>");
+document.write("<br>");
+
+let home={
+	model: "karala",
+	sape:"round",
+	color: "lightgray",
+	ground:3,
+	design:"art",
+	amount: 200000,
+	
+}
+document.write(JSON.stringify(home));
+console.log(home);
+console.log(home.model);
+console.log(home["amount"]);
